20ad6b6ec96187d173c34b77621d134865a5aebc
Geronimo <cortigeronimo@gmail.com>
2021-11-17 21:02:18 -0300

    Agregamos la queue normal (con su estado) que por el momento, agrega un consumidor y obtiene los consumidores

---
+++ tp_final/lib/model/normal_queue.ex
@@ -0,0 +1,39 @@
+defmodule NormalQueue do
+	use GenServer
+
+	def start_link() do
+		GenServer.start_link(__MODULE__)
+	end
+
+	def init({consumers}) do
+    {:ok, {consumers}}
+  end
+
+	def handle_call(:healthCheck, _from, {consumers}) do
+		{:reply, :healthCheck, {consumers}}
+	end
+
+	def handle_call({:addConsumer, consumer}, _from, {consumers}) do
+		{:reply, :ok, {consumers ++ [consumer]}}
+	end
+
+	def handle_call(:getConsumers, _from, {consumers}) do
+		{:reply, consumers, {consumers}}
+	end
+
+	def handle_cast({:processMessage, message}, {consumers}) do
+		{:noreply, {consumers}}
+	end
+
+end
+
+"""
+	iex
+	c("normal_queue.ex")
+	{:ok, pid} = GenServer.start_link(NormalQueue, {[]})
+	GenServer.call(pid, {:addConsumer, :jorge})
+	GenServer.call(pid, {:addConsumer, :rama})
+	GenServer.call(pid, {:addConsumer, :berko})
+	GenServer.call(pid, :getConsumers)
+
+"""
