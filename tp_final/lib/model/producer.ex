e0fa7be1b1833904a3118d58ee2a3d5ffc2d5934
Gonzalo Maidan <gonzalo_maidan@hotmail.com>
2021-12-06 22:31:38 -0300

    produce n mensajes

--- tp_final/lib/model/producer.ex
+++ tp_final/lib/model/producer.ex
@@ -12,4 +12,12 @@ defmodule Producer do
 		send_message(queue_name, {id, {:wait, ms}, message})
 	end
 
+	def produce_n_wait_messages(queue_name, prefix_id, message, n, ms, delay_between_messages) do
+		produce_wait_message(queue_name, "#{prefix_id}_#{n}", message, ms)
+		if n > 0 do
+			Process.sleep(delay_between_messages)
+			produce_n_wait_messages(queue_name, prefix_id,  message, n - 1, ms, delay_between_messages)
+		end
+	end
+
 end
